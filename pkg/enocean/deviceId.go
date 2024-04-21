cec2a7825017ce6fb13eb9cd4d08684d5abfcbef
Edvard LUNDIN <edvard.lundin@pm.me>
2024-04-21 14:59:37 +0200

    feat(enocean): deviceId serialization and parsing with tests
    Signed-off-by: Edvard LUNDIN <edvard.lundin@pm.me>

--- pkg/enocean/deviceId.go
+++ pkg/enocean/deviceId.go
@@ -3,11 +3,15 @@ package enocean
 import (
 	"encoding/binary"
 	"encoding/hex"
-	"errors"
 	"fmt"
+	"strings"
 )
 
 type DeviceId uint32
+
+const (
+	SIZE_DEVICE_ID int = 4
+)
 
 func GetBroadcastId() DeviceId {
 	return 0xffffffff
@@ -18,19 +22,39 @@ func (d DeviceId) String() string {
 }
 
 func DeviceIdFromHexString(s string) (DeviceId, error) {
-	b, err := hex.DecodeString(s)
+	const SIZE_MAX_STR = SIZE_DEVICE_ID * 2
+
+	stringToDecode := s
+
+	if strings.HasPrefix(stringToDecode, "0x") {
+		stringToDecode = s[2:]
+	}
+
+	if len(stringToDecode) > SIZE_MAX_STR {
+		return 0, fmt.Errorf("invalid length (got %d, max: %d)", len(stringToDecode), SIZE_MAX_STR)
+	}
+
+	if len(stringToDecode)%2 != 0 {
+		stringToDecode = strings.Repeat("0", 1) + stringToDecode
+	}
+
+	b, err := hex.DecodeString(stringToDecode)
 
 	if err != nil {
 		return 0, err
 	}
 
-	if len(b) != 4 {
-		return 0, errors.New("invalid length")
-	}
-
-	return DeviceIdFromByteArray(b), nil
+	return DeviceIdFromByteArray(b)
 }
 
-func DeviceIdFromByteArray(b []byte) DeviceId {
-	return DeviceId(binary.BigEndian.Uint32(b[0:4]))
+func DeviceIdFromByteArray(b []byte) (DeviceId, error) {
+	if len(b) > SIZE_DEVICE_ID {
+		return 0, fmt.Errorf("invalid length (got %d, need: %d)", len(b), SIZE_DEVICE_ID)
+	}
+
+	if len(b) < SIZE_DEVICE_ID {
+		b = append(make([]byte, SIZE_DEVICE_ID-len(b)), b...)
+	}
+
+	return DeviceId(binary.BigEndian.Uint32(b[0:]) >> (32 - SIZE_DEVICE_ID*8)), nil
 }
