ed5d60c08e2c9f79693ad7604b931eeafc4d820b
Edvard LUNDIN <edvard.lundin@pm.me>
2025-07-30 21:46:05 +0200

    feat(enums): implement ReturnCode enum

---
+++ pkg/enums/return_code.go
@@ -0,0 +1,62 @@
+package enums
+
+import "errors"
+
+type ReturnCode byte
+
+const (
+	ReturnCodeSUCCESS ReturnCode = iota
+	ReturnCodeERROR
+	ReturnCodeNOT_SUPPORTED
+	ReturnCodeWRONG_ARGUMENT
+	ReturnCodeOPERATION_DENIED
+	ReturnCodeLOCK_SET
+	ReturnCodeBUFFER_TO_SMALL
+	ReturnCodeNO_FREE_BUFFER
+)
+
+func ParseReturnCodeFromByte(byte uint8) (ReturnCode, error) {
+	switch byte {
+	case 0x00:
+		return ReturnCodeSUCCESS, nil
+	case 0x01:
+		return ReturnCodeERROR, nil
+	case 0x02:
+		return ReturnCodeNOT_SUPPORTED, nil
+	case 0x03:
+		return ReturnCodeWRONG_ARGUMENT, nil
+	case 0x04:
+		return ReturnCodeOPERATION_DENIED, nil
+	case 0x05:
+		return ReturnCodeLOCK_SET, nil
+	case 0x06:
+		return ReturnCodeBUFFER_TO_SMALL, nil
+	case 0x07:
+		return ReturnCodeNO_FREE_BUFFER, nil
+	default:
+		return ReturnCodeERROR, errors.New("invalid return code")
+	}
+}
+
+func (returnCode ReturnCode) String() string {
+	switch returnCode {
+	case ReturnCodeSUCCESS:
+		return "SUCCESS"
+	case ReturnCodeERROR:
+		return "ERROR"
+	case ReturnCodeNOT_SUPPORTED:
+		return "NOT_SUPPORTED"
+	case ReturnCodeWRONG_ARGUMENT:
+		return "WRONG_ARGUMENT"
+	case ReturnCodeOPERATION_DENIED:
+		return "OPERATION_DENIED"
+	case ReturnCodeLOCK_SET:
+		return "LOCK_SET"
+	case ReturnCodeBUFFER_TO_SMALL:
+		return "BUFFER_TO_SMALL"
+	case ReturnCodeNO_FREE_BUFFER:
+		return "NO_FREE_BUFFER"
+	default:
+		return "UNKNOWN"
+	}
+}
